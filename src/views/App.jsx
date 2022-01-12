e39edc250e5e73050564feb54e5b963f97ab9f7e
A. Canyon <acanyon@gmail.com>
2022-01-11 21:33:04 -0800

    updated stuff

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import cn from 'classnames';
 
 import Home from './Home';
+import Events from './Events';
 import NoRoute404 from './NoRoute404';
 import {
   BrowserRouter as Router,
@@ -22,17 +24,15 @@ class App extends Component {
         <Router>
           <div className={styles.header}>
             <div className={styles.container}>
-              <Link to="/"><i className="fas fa-kiss-wink-heart"></i></Link>
+              <Link to="/"><i className={cn("fas fa-kiss-wink-heart", styles.logo)}></i></Link>
+                Dough Dating
             </div>
           </div>
           <div className={styles.content}>
             <Routes>
-              <Route exact path="/" element={<Home />}/>
+              <Route exact path="/" element={<Events />}/>
               <Route element={<NoRoute404 />}/>
             </Routes>
-          </div>
-          <div className={styles.footer}>
-            <i className="fas fa-kiss-wink-heart"></i>
           </div>
         </Router>
       </div>
