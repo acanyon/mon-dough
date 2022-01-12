7be8f6e0e5e70595ca8b8fb1fe3151526b32bb65
A. Canyon <acanyon@gmail.com>
2022-01-11 22:01:54 -0800

    fix

--- src/views/Events.jsx
+++ src/views/Events.jsx
@@ -6,17 +6,28 @@ import styles from './Events.scss';
 const EVENTS = [
   {
     id: 1,
+    eventName: 'Tech Asks You to Bring a Friend',
     barPhoto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bar-P1030319.jpg/1200px-Bar-P1030319.jpg',
     tags: ['Plus one welcome', '20-30 yrs old'],
     eventDate: 'Wed Jan 19 - 6:30pm',
     eventLocationName: 'Whitechapel',
     eventAddress: '600 Polk St, San Francisco, CA 94102',
     rsvpCount: 23,
-  },
-  {
+  }, {
     id: 2,
+    eventName: 'Single Marketing Professionals',
     barPhoto: 'https://d3h1lg3ksw6i6b.cloudfront.net/media/image/2019/08/14/97f04adefa11426fa88d78611b9e1133_web-Harvard_and_Stone-14_lax_credit_harvardandstone_INLINE.jpg',
     tags: ['Singles only', 'Marketing professionals'],
+    eventDate: 'Thurs Jan 20 - 6:30pm',
+    eventLocationName: 'Hi-Lo Club',
+    eventAddress: '1423 Polk St, San Francisco, CA 94109',
+    rsvpCount: 16,
+  }, {
+    id: 3,
+    eventName: 'Group Gathering',
+    waitlist: true,
+    barPhoto: 'https://cdn.cnn.com/cnnnext/dam/assets/200622142705-suspended-texas-bars-restricted-super-tease.jpg',
+    tags: ["Bring yo' crew"],
     eventDate: 'Thurs Jan 20 - 6:30pm',
     eventLocationName: 'Hi-Lo Club',
     eventAddress: '1423 Polk St, San Francisco, CA 94109',
@@ -38,9 +49,18 @@ class Events extends Component {
         <div className={styles.eventContainer}>
           <div style={{backgroundImage: `url(${props.barPhoto})`}} className={styles.eventPhoto}></div>
           <div className={styles.eventInfo}>
-            <div className={styles.header}>{props.eventLocationName}</div>
+            <div className={styles.header}>
+              {`${props.eventName} @ ${props.eventLocationName}`}
+            </div>
             <div className={styles.tagContainer}>
               {props.tags.map((tag) => <div className={styles.tag} key={tag}>{tag}</div>)}
+            </div>
+            <div className={styles.fillSpace}></div>
+            <div className={styles.footer}>
+              <div className={styles.rsvpCount}>
+                {props.waitlist ? 'SOLD OUT' : `${props.rsvpCount} attending`}
+              </div>
+              <div className={styles.rsvpButton}>{props.waitlist ? 'Waitlst' : 'RSVP'}</div>
             </div>
           </div>
           <div className={styles.chevron}><i className="fas fa-chevron-right"></i></div>
